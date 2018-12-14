9228167e2cedacdc36f553bd58dd5ca965d2ca01
wyrover <wyrover@gmail.com>
2018-12-14 11:46:06 +0800

    * index.js

--- src/index.js
+++ src/index.js
@@ -8,314 +8,271 @@ import 'echarts/lib/component/toolbox'
 import 'echarts/lib/chart/candlestick'
 import 'echarts/lib/component/dataZoom'
 
-var myChart = echarts.init(document.getElementById('main'))
-// 绘制图表
-myChart.setOption({
-  title: {
-    text: 'ECharts 入门示例'
-  },
-  tooltip: {},
-  xAxis: {
-    data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
-  },
-  yAxis: {},
-  series: [
-    {
-      name: '销量',
-      type: 'bar',
-      data: [5, 20, 36, 10, 10, 20]
-    }
-  ]
-})
-
-echarts.init(document.getElementById('main2')).setOption({
-  title: { text: 'Line Chart' },
-  tooltip: {},
-  toolbox: {
-    feature: {
-      dataView: {},
-      saveAsImage: {
-        pixelRatio: 2
-      },
-      restore: {}
-    }
-  },
-  xAxis: {},
-  yAxis: {},
-  series: [
-    {
-      type: 'line',
-      smooth: true,
-      data: [[12, 5], [24, 20], [36, 36], [48, 10], [60, 10], [72, 20]]
-    }
-  ]
-})
-
-var upColor = '#ec0000'
-var upBorderColor = '#8A0000'
-var downColor = '#00da3c'
-var downBorderColor = '#008F28'
-
-// 数据意义：开盘(open)，收盘(close)，最低(lowest)，最高(highest)
-var data0 = splitData([
-  ['2013/1/24', 2320.26, 2320.26, 2287.3, 2362.94],
-  ['2013/1/25', 2300, 2291.3, 2288.26, 2308.38],
-  ['2013/1/28', 2295.35, 2346.5, 2295.35, 2346.92],
-  ['2013/1/29', 2347.22, 2358.98, 2337.35, 2363.8],
-  ['2013/1/30', 2360.75, 2382.48, 2347.89, 2383.76],
-  ['2013/1/31', 2383.43, 2385.42, 2371.23, 2391.82],
-  ['2013/2/1', 2377.41, 2419.02, 2369.57, 2421.15],
-  ['2013/2/4', 2425.92, 2428.15, 2417.58, 2440.38],
-  ['2013/2/5', 2411, 2433.13, 2403.3, 2437.42],
-  ['2013/2/6', 2432.68, 2434.48, 2427.7, 2441.73],
-  ['2013/2/7', 2430.69, 2418.53, 2394.22, 2433.89],
-  ['2013/2/8', 2416.62, 2432.4, 2414.4, 2443.03],
-  ['2013/2/18', 2441.91, 2421.56, 2415.43, 2444.8],
-  ['2013/2/19', 2420.26, 2382.91, 2373.53, 2427.07],
-  ['2013/2/20', 2383.49, 2397.18, 2370.61, 2397.94],
-  ['2013/2/21', 2378.82, 2325.95, 2309.17, 2378.82],
-  ['2013/2/22', 2322.94, 2314.16, 2308.76, 2330.88],
-  ['2013/2/25', 2320.62, 2325.82, 2315.01, 2338.78],
-  ['2013/2/26', 2313.74, 2293.34, 2289.89, 2340.71],
-  ['2013/2/27', 2297.77, 2313.22, 2292.03, 2324.63],
-  ['2013/2/28', 2322.32, 2365.59, 2308.92, 2366.16],
-  ['2013/3/1', 2364.54, 2359.51, 2330.86, 2369.65],
-  ['2013/3/4', 2332.08, 2273.4, 2259.25, 2333.54],
-  ['2013/3/5', 2274.81, 2326.31, 2270.1, 2328.14],
-  ['2013/3/6', 2333.61, 2347.18, 2321.6, 2351.44],
-  ['2013/3/7', 2340.44, 2324.29, 2304.27, 2352.02],
-  ['2013/3/8', 2326.42, 2318.61, 2314.59, 2333.67],
-  ['2013/3/11', 2314.68, 2310.59, 2296.58, 2320.96],
-  ['2013/3/12', 2309.16, 2286.6, 2264.83, 2333.29],
-  ['2013/3/13', 2282.17, 2263.97, 2253.25, 2286.33],
-  ['2013/3/14', 2255.77, 2270.28, 2253.31, 2276.22],
-  ['2013/3/15', 2269.31, 2278.4, 2250, 2312.08],
-  ['2013/3/18', 2267.29, 2240.02, 2239.21, 2276.05],
-  ['2013/3/19', 2244.26, 2257.43, 2232.02, 2261.31],
-  ['2013/3/20', 2257.74, 2317.37, 2257.42, 2317.86],
-  ['2013/3/21', 2318.21, 2324.24, 2311.6, 2330.81],
-  ['2013/3/22', 2321.4, 2328.28, 2314.97, 2332],
-  ['2013/3/25', 2334.74, 2326.72, 2319.91, 2344.89],
-  ['2013/3/26', 2318.58, 2297.67, 2281.12, 2319.99],
-  ['2013/3/27', 2299.38, 2301.26, 2289, 2323.48],
-  ['2013/3/28', 2273.55, 2236.3, 2232.91, 2273.55],
-  ['2013/3/29', 2238.49, 2236.62, 2228.81, 2246.87],
-  ['2013/4/1', 2229.46, 2234.4, 2227.31, 2243.95],
-  ['2013/4/2', 2234.9, 2227.74, 2220.44, 2253.42],
-  ['2013/4/3', 2232.69, 2225.29, 2217.25, 2241.34],
-  ['2013/4/8', 2196.24, 2211.59, 2180.67, 2212.59],
-  ['2013/4/9', 2215.47, 2225.77, 2215.47, 2234.73],
-  ['2013/4/10', 2224.93, 2226.13, 2212.56, 2233.04],
-  ['2013/4/11', 2236.98, 2219.55, 2217.26, 2242.48],
-  ['2013/4/12', 2218.09, 2206.78, 2204.44, 2226.26],
-  ['2013/4/15', 2199.91, 2181.94, 2177.39, 2204.99],
-  ['2013/4/16', 2169.63, 2194.85, 2165.78, 2196.43],
-  ['2013/4/17', 2195.03, 2193.8, 2178.47, 2197.51],
-  ['2013/4/18', 2181.82, 2197.6, 2175.44, 2206.03],
-  ['2013/4/19', 2201.12, 2244.64, 2200.58, 2250.11],
-  ['2013/4/22', 2236.4, 2242.17, 2232.26, 2245.12],
-  ['2013/4/23', 2242.62, 2184.54, 2182.81, 2242.62],
-  ['2013/4/24', 2187.35, 2218.32, 2184.11, 2226.12],
-  ['2013/4/25', 2213.19, 2199.31, 2191.85, 2224.63],
-  ['2013/4/26', 2203.89, 2177.91, 2173.86, 2210.58],
-  ['2013/5/2', 2170.78, 2174.12, 2161.14, 2179.65],
-  ['2013/5/3', 2179.05, 2205.5, 2179.05, 2222.81],
-  ['2013/5/6', 2212.5, 2231.17, 2212.5, 2236.07],
-  ['2013/5/7', 2227.86, 2235.57, 2219.44, 2240.26],
-  ['2013/5/8', 2242.39, 2246.3, 2235.42, 2255.21],
-  ['2013/5/9', 2246.96, 2232.97, 2221.38, 2247.86],
-  ['2013/5/10', 2228.82, 2246.83, 2225.81, 2247.67],
-  ['2013/5/13', 2247.68, 2241.92, 2231.36, 2250.85],
-  ['2013/5/14', 2238.9, 2217.01, 2205.87, 2239.93],
-  ['2013/5/15', 2217.09, 2224.8, 2213.58, 2225.19],
-  ['2013/5/16', 2221.34, 2251.81, 2210.77, 2252.87],
-  ['2013/5/17', 2249.81, 2282.87, 2248.41, 2288.09],
-  ['2013/5/20', 2286.33, 2299.99, 2281.9, 2309.39],
-  ['2013/5/21', 2297.11, 2305.11, 2290.12, 2305.3],
-  ['2013/5/22', 2303.75, 2302.4, 2292.43, 2314.18],
-  ['2013/5/23', 2293.81, 2275.67, 2274.1, 2304.95],
-  ['2013/5/24', 2281.45, 2288.53, 2270.25, 2292.59],
-  ['2013/5/27', 2286.66, 2293.08, 2283.94, 2301.7],
-  ['2013/5/28', 2293.4, 2321.32, 2281.47, 2322.1],
-  ['2013/5/29', 2323.54, 2324.02, 2321.17, 2334.33],
-  ['2013/5/30', 2316.25, 2317.75, 2310.49, 2325.72],
-  ['2013/5/31', 2320.74, 2300.59, 2299.37, 2325.53],
-  ['2013/6/3', 2300.21, 2299.25, 2294.11, 2313.43],
-  ['2013/6/4', 2297.1, 2272.42, 2264.76, 2297.1],
-  ['2013/6/5', 2270.71, 2270.93, 2260.87, 2276.86],
-  ['2013/6/6', 2264.43, 2242.11, 2240.07, 2266.69],
-  ['2013/6/7', 2242.26, 2210.9, 2205.07, 2250.63],
-  ['2013/6/13', 2190.1, 2148.35, 2126.22, 2190.1]
+//数据模型 time0 open1 close2 min3 max4 vol5 tag6 macd7 dif8 dea9
+//['2015-10-19',18.56,18.25,18.19,18.56,55.00,0,-0.00,0.08,0.09]
+var data = splitData([
+  ['2015-10-16', 18.4, 18.58, 18.33, 18.79, 67.0, 1, 0.04, 0.11, 0.09],
+  ['2015-10-19', 18.56, 18.25, 18.19, 18.56, 55.0, 0, -0.0, 0.08, 0.09],
+  ['2015-10-20', 18.3, 18.22, 18.05, 18.41, 37.0, 0, 0.01, 0.09, 0.09],
+  ['2015-10-21', 18.18, 18.69, 18.02, 18.98, 89.0, 0, 0.03, 0.1, 0.08],
+  ['2015-10-22', 18.42, 18.29, 18.22, 18.48, 43.0, 0, -0.06, 0.05, 0.08],
+  ['2015-10-23', 18.26, 18.19, 18.08, 18.36, 46.0, 0, -0.1, 0.03, 0.09],
+  ['2015-10-26', 18.33, 18.07, 17.98, 18.35, 65.0, 0, -0.15, 0.03, 0.1],
+  ['2015-10-27', 18.08, 18.04, 17.88, 18.13, 37.0, 0, -0.19, 0.03, 0.12],
+  ['2015-10-28', 17.96, 17.86, 17.82, 17.99, 35.0, 0, -0.24, 0.03, 0.15],
+  ['2015-10-29', 17.85, 17.81, 17.8, 17.93, 27.0, 0, -0.24, 0.06, 0.18],
+  ['2015-10-30', 17.79, 17.93, 17.78, 18.08, 43.0, 0, -0.22, 0.11, 0.22],
+  ['2015-11-02', 17.78, 17.83, 17.78, 18.04, 27.0, 0, -0.2, 0.15, 0.25],
+  ['2015-11-03', 17.84, 17.9, 17.84, 18.06, 34.0, 0, -0.12, 0.22, 0.28],
+  ['2015-11-04', 17.97, 18.36, 17.85, 18.39, 62.0, 0, -0.0, 0.3, 0.3],
+  ['2015-11-05', 18.3, 18.57, 18.18, 19.08, 177.0, 0, 0.07, 0.33, 0.3],
+  ['2015-11-06', 18.53, 18.68, 18.3, 18.71, 95.0, 0, 0.12, 0.35, 0.29],
+  ['2015-11-09', 18.75, 19.08, 18.75, 19.98, 202.0, 1, 0.16, 0.35, 0.27],
+  ['2015-11-10', 18.85, 18.64, 18.56, 18.99, 85.0, 0, 0.09, 0.29, 0.25],
+  ['2015-11-11', 18.64, 18.44, 18.31, 18.64, 50.0, 0, 0.06, 0.27, 0.23],
+  ['2015-11-12', 18.55, 18.27, 18.17, 18.57, 43.0, 0, 0.05, 0.25, 0.23],
+  ['2015-11-13', 18.13, 18.14, 18.09, 18.34, 35.0, 0, 0.05, 0.24, 0.22],
+  ['2015-11-16', 18.01, 18.1, 17.93, 18.17, 34.0, 0, 0.07, 0.25, 0.21],
+  ['2015-11-17', 18.2, 18.14, 18.08, 18.45, 58.0, 0, 0.11, 0.25, 0.2],
+  ['2015-11-18', 18.23, 18.16, 18.0, 18.45, 47.0, 0, 0.13, 0.25, 0.19],
+  ['2015-11-19', 18.08, 18.2, 18.05, 18.25, 32.0, 0, 0.15, 0.24, 0.17],
+  ['2015-11-20', 18.15, 18.15, 18.11, 18.29, 36.0, 0, 0.13, 0.21, 0.15],
+  ['2015-11-23', 18.16, 18.19, 18.12, 18.34, 47.0, 0, 0.11, 0.18, 0.13],
+  ['2015-11-24', 18.23, 17.88, 17.7, 18.23, 62.0, 0, 0.03, 0.13, 0.11],
+  ['2015-11-25', 17.85, 17.73, 17.56, 17.85, 66.0, 0, -0.03, 0.09, 0.11],
+  ['2015-11-26', 17.79, 17.53, 17.5, 17.92, 63.0, 0, -0.1, 0.06, 0.11],
+  ['2015-11-27', 17.51, 17.04, 16.9, 17.51, 67.0, 0, -0.16, 0.05, 0.13],
+  ['2015-11-30', 17.07, 17.2, 16.98, 17.32, 55.0, 0, -0.12, 0.09, 0.15]
 ])
-
+//数组处理
 function splitData(rawData) {
-  var categoryData = []
-  var values = []
+  var datas = []
+  var times = []
+  var vols = []
+  var macds = []
+  var difs = []
+  var deas = []
   for (var i = 0; i < rawData.length; i++) {
-    categoryData.push(rawData[i].splice(0, 1)[0])
-    values.push(rawData[i])
+    datas.push(rawData[i])
+    times.push(rawData[i].splice(0, 1)[0])
+    vols.push(rawData[i][4])
+    macds.push(rawData[i][6])
+    difs.push(rawData[i][7])
+    deas.push(rawData[i][8])
   }
   return {
-    categoryData: categoryData,
-    values: values
+    datas: datas,
+    times: times,
+    vols: vols,
+    macds: macds,
+    difs: difs,
+    deas: deas
   }
 }
 
+//分段计算
+function fenduans() {
+  var markLineData = []
+  var idx = 0
+  var tag = 0
+  var vols = 0
+  for (var i = 0; i < data.times.length; i++) {
+    //初始化数据
+    if (data.datas[i][5] != 0 && tag == 0) {
+      idx = i
+      vols = data.datas[i][4]
+      tag = 1
+    }
+    if (tag == 1) {
+      vols += data.datas[i][4]
+    }
+    if (data.datas[i][5] != 0 && tag == 1) {
+      markLineData.push([
+        {
+          xAxis: idx,
+          yAxis:
+            data.datas[idx][1] > data.datas[idx][0]
+              ? data.datas[idx][3].toFixed(2)
+              : data.datas[idx][2].toFixed(2),
+          value: vols
+        },
+        {
+          xAxis: i,
+          yAxis:
+            data.datas[i][1] > data.datas[i][0]
+              ? data.datas[i][3].toFixed(2)
+              : data.datas[i][2].toFixed(2)
+        }
+      ])
+      idx = i
+      vols = data.datas[i][4]
+      tag = 2
+    }
+
+    //更替数据
+    if (tag == 2) {
+      vols += data.datas[i][4]
+    }
+    if (data.datas[i][5] != 0 && tag == 2) {
+      markLineData.push([
+        {
+          xAxis: idx,
+          yAxis:
+            data.datas[idx][1] > data.datas[idx][0]
+              ? data.datas[idx][3].toFixed(2)
+              : data.datas[idx][2].toFixed(2),
+          value: (vols / (i - idx + 1)).toFixed(2) + ' M'
+        },
+        {
+          xAxis: i,
+          yAxis:
+            data.datas[i][1] > data.datas[i][0]
+              ? data.datas[i][3].toFixed(2)
+              : data.datas[i][2].toFixed(2)
+        }
+      ])
+      idx = i
+      vols = data.datas[i][4]
+    }
+  }
+  return markLineData
+}
+
+//MA计算公式
 function calculateMA(dayCount) {
   var result = []
-  for (var i = 0, len = data0.values.length; i < len; i++) {
+  for (var i = 0, len = data.times.length; i < len; i++) {
     if (i < dayCount) {
       result.push('-')
       continue
     }
     var sum = 0
     for (var j = 0; j < dayCount; j++) {
-      sum += data0.values[i - j][1]
+      sum += data.datas[i - j][1]
     }
-    result.push(sum / dayCount)
+    result.push((sum / dayCount).toFixed(2))
   }
   return result
 }
 
-let option = {
+var option = {
   title: {
-    text: '上证指数',
+    text: 'K线周期图表',
     left: 0
   },
   tooltip: {
     trigger: 'axis',
     axisPointer: {
-      type: 'cross'
+      type: 'line'
     }
   },
   legend: {
-    data: ['日K', 'MA5', 'MA10', 'MA20', 'MA30']
+    data: ['KLine', 'MA5']
   },
-  grid: {
-    left: '10%',
-    right: '10%',
-    bottom: '15%'
-  },
-  xAxis: {
-    type: 'category',
-    data: data0.categoryData,
-    scale: true,
-    boundaryGap: false,
-    axisLine: { onZero: false },
-    splitLine: { show: false },
-    splitNumber: 20,
-    min: 'dataMin',
-    max: 'dataMax'
-  },
-  yAxis: {
-    scale: true,
-    splitArea: {
-      show: true
+  grid: [
+    {
+      left: '3%',
+      right: '1%',
+      height: '60%'
+    },
+    {
+      left: '3%',
+      right: '1%',
+      top: '71%',
+      height: '10%'
+    },
+    {
+      left: '3%',
+      right: '1%',
+      top: '82%',
+      height: '14%'
     }
-  },
+  ],
+  xAxis: [
+    {
+      type: 'category',
+      data: data.times,
+      scale: true,
+      boundaryGap: false,
+      axisLine: { onZero: false },
+      splitLine: { show: false },
+      splitNumber: 20,
+      min: 'dataMin',
+      max: 'dataMax'
+    },
+    {
+      type: 'category',
+      gridIndex: 1,
+      data: data.times,
+      axisLabel: { show: false }
+    },
+    {
+      type: 'category',
+      gridIndex: 2,
+      data: data.times,
+      axisLabel: { show: false }
+    }
+  ],
+  yAxis: [
+    {
+      scale: true,
+      splitArea: {
+        show: false
+      }
+    },
+    {
+      gridIndex: 1,
+      splitNumber: 3,
+      axisLine: { onZero: false },
+      axisTick: { show: false },
+      splitLine: { show: false },
+      axisLabel: { show: true }
+    },
+    {
+      gridIndex: 2,
+      splitNumber: 4,
+      axisLine: { onZero: false },
+      axisTick: { show: false },
+      splitLine: { show: false },
+      axisLabel: { show: true }
+    }
+  ],
   dataZoom: [
     {
       type: 'inside',
-      start: 50,
+      xAxisIndex: [0, 0],
+      start: 20,
       end: 100
     },
     {
       show: true,
+      xAxisIndex: [0, 1],
       type: 'slider',
-      y: '90%',
-      start: 50,
+      top: '97%',
+      start: 20,
+      end: 100
+    },
+    {
+      show: false,
+      xAxisIndex: [0, 2],
+      type: 'slider',
+      start: 20,
       end: 100
     }
   ],
+
   series: [
     {
-      name: '日K',
+      name: 'K线周期图表',
       type: 'candlestick',
-      data: data0.values,
+      data: data.datas,
       itemStyle: {
         normal: {
-          color: upColor,
-          color0: downColor,
-          borderColor: upBorderColor,
-          borderColor0: downBorderColor
+          color: '#ef232a',
+          color0: '#14b143',
+          borderColor: '#ef232a',
+          borderColor0: '#14b143'
         }
-      },
-      markPoint: {
-        label: {
-          normal: {
-            formatter: function(param) {
-              return param != null ? Math.round(param.value) : ''
-            }
-          }
-        },
-        data: [
-          {
-            name: 'XX标点',
-            coord: ['2013/5/31', 2300],
-            value: 2300,
-            itemStyle: {
-              normal: { color: 'rgb(41,60,85)' }
-            }
-          },
-          {
-            name: 'highest value',
-            type: 'max',
-            valueDim: 'highest'
-          },
-          {
-            name: 'lowest value',
-            type: 'min',
-            valueDim: 'lowest'
-          },
-          {
-            name: 'average value on close',
-            type: 'average',
-            valueDim: 'close'
-          }
-        ],
-        tooltip: {
-          formatter: function(param) {
-            return param.name + '<br>' + (param.data.coord || '')
-          }
-        }
-      },
-      markLine: {
-        symbol: ['none', 'none'],
-        data: [
-          [
-            {
-              name: 'from lowest to highest',
-              type: 'min',
-              valueDim: 'lowest',
-              symbol: 'circle',
-              symbolSize: 10,
-              label: {
-                normal: { show: false },
-                emphasis: { show: false }
-              }
-            },
-            {
-              type: 'max',
-              valueDim: 'highest',
-              symbol: 'circle',
-              symbolSize: 10,
-              label: {
-                normal: { show: false },
-                emphasis: { show: false }
-              }
-            }
-          ],
-          {
-            name: 'min line on close',
-            type: 'min',
-            valueDim: 'close'
-          },
-          {
-            name: 'max line on close',
-            type: 'max',
-            valueDim: 'close'
-          }
-        ]
       }
     },
     {
@@ -324,37 +281,68 @@ let option = {
       data: calculateMA(5),
       smooth: true,
       lineStyle: {
-        normal: { opacity: 0.5 }
+        normal: {
+          opacity: 0.5
+        }
       }
     },
     {
-      name: 'MA10',
-      type: 'line',
-      data: calculateMA(10),
-      smooth: true,
-      lineStyle: {
-        normal: { opacity: 0.5 }
+      name: 'Volumn',
+      type: 'bar',
+      xAxisIndex: 1,
+      yAxisIndex: 1,
+      data: data.vols,
+      itemStyle: {
+        normal: {
+          color: function(params) {
+            var colorList
+            if (
+              data.datas[params.dataIndex][1] > data.datas[params.dataIndex][0]
+            ) {
+              colorList = '#ef232a'
+            } else {
+              colorList = '#14b143'
+            }
+            return colorList
+          }
+        }
       }
     },
     {
-      name: 'MA20',
-      type: 'line',
-      data: calculateMA(20),
-      smooth: true,
-      lineStyle: {
-        normal: { opacity: 0.5 }
+      name: 'MACD',
+      type: 'bar',
+      xAxisIndex: 2,
+      yAxisIndex: 2,
+      data: data.macds,
+      itemStyle: {
+        normal: {
+          color: function(params) {
+            var colorList
+            if (params.data >= 0) {
+              colorList = '#ef232a'
+            } else {
+              colorList = '#14b143'
+            }
+            return colorList
+          }
+        }
       }
     },
     {
-      name: 'MA30',
+      name: 'DIF',
       type: 'line',
-      data: calculateMA(30),
-      smooth: true,
-      lineStyle: {
-        normal: { opacity: 0.5 }
-      }
+      xAxisIndex: 2,
+      yAxisIndex: 2,
+      data: data.difs
+    },
+    {
+      name: 'DEA',
+      type: 'line',
+      xAxisIndex: 2,
+      yAxisIndex: 2,
+      data: data.deas
     }
   ]
 }
 
-echarts.init(document.getElementById('main3')).setOption(option)
+echarts.init(document.getElementById('main')).setOption(option)
